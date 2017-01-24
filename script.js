325cb6e6f79072b0676740a0f65786cc8eaeb718
Danny Vega <d_vega@apple.com>
2017-01-24 11:57:48 -0800

    made use of a closure for keeping score function

--- script.js
+++ script.js
@@ -17,30 +17,56 @@
 		}
 	}
 
-	Question.prototype.checkUserAnswer = function(ans){
+// passing the callback to this function is returning the 'score' value from inside the calculateScore function -- need to save that returned value into a variable to use later
+// after saving 'score' value into new variable within this method, pass another function to display the score
+	Question.prototype.checkUserAnswer = function(ans, callback){
+		var scr;
 		if(ans === this.answer){
-			console.log("-----------------------");
+			console.log("-------------------------");
 			console.log("Correct!");
-			console.log("-----------------------");
+			console.log("-------------------------");
+			scr = callback(true);
 		} else {
-			console.log("-----------------------");
+			console.log("-------------------------");
 			console.log("Better luck next time.");
-			console.log("-----------------------");
+			console.log("-------------------------");
+			scr = callback(false);
 		}
+		this.displayScore(scr);
+	}
+
+	Question.prototype.displayScore = function(score){
+		console.log("Your current score is: " + score);
+		console.log("-------------------------");
 	}
 
 	function nextQuestion(){
 		var random = Math.floor((Math.random() * questionsArray.length));
 		var currentQuestion = questionsArray[random];
 		currentQuestion.displayQuestion();
-		var userInput = prompt("Please enter the correct answer to question in the console:");
+		var userInput = prompt("Please enter the correct answer to question in the console. Enter 'exit' to quit the game." );
 		if(userInput !== "exit"){
-			currentQuestion.checkUserAnswer(parseInt(userInput));
+			currentQuestion.checkUserAnswer(parseInt(userInput), runningScore);
 			nextQuestion();
 		} else {
+			console.log("-------------------------");
 			console.log("Thanks for playing!");
+			console.log("-------------------------");
 		}
 	}
+
+	function calculateScore(){
+		var score = 0;
+		return function(correct){
+			if(correct){
+				score++;
+			}
+			return score;
+		}
+	}
+
+// return the inner function and save it in a variable to use later
+	var runningScore = calculateScore();
 
 	nextQuestion();
 })();
